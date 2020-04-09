f22326106e3c3a60a2504b714647f83d77a1c1fa
gongguanzhong <9261904@qq.com>
2020-04-09 10:20:41 +0800

    updata

--- jiekou_unittest/run_all_case1.py
+++ jiekou_unittest/run_all_case1.py
@@ -9,7 +9,7 @@ now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
 
 def all_case():
     # 待执行的目录
-    case_dir = "E:\\jiekou-test\\jiekou_unittest\\case1"
+    case_dir = "./case1"
     testcase = unittest.TestSuite()
     discover = unittest.defaultTestLoader.discover(case_dir, pattern="test*.py", top_level_dir=None)
     testcase.addTests(discover)  # 直接加载discover
@@ -20,7 +20,7 @@ def all_case():
 if __name__ == "__main__":
     # 返回实例
     runner = unittest.TextTestRunner()
-    report_path = "E:\\report\\" + now + 'result.html'
+    report_path = "./" + now + 'result.html'
     fp = open(report_path, "wb")
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u'这是我的测试报告', description=u'用例执行情况：')
     # 返回所有用例
